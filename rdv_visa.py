c7c2c27078edbe724d3e1c1f8d50a80c00c71942
Ever Atilano <ever.developer3001@gmail.com>
2020-08-05 15:35:24 +0200

    Script V0.2.0

--- rdv_visa.py
+++ rdv_visa.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+## All data for the form
 data = {
     "nom"           : "Pierre",         # Nom (e.g. Pierre)
     "prenom"        : "KONG",           # Prenom (e.g. KONG)
@@ -12,44 +17,52 @@ data = {
     "nationalite"   : "MEXICAINE",      # Nationalité (e.g. MEXICAINE)
     "arrdt"         : "12",             # Arrondissement (e.g. 12)
     "time_in_secs"  : 30                # Temps pour reviser la page web en secondes
+    "url_video"     : "https://youtu.be/04854XqcfCY?t=39"   # Lien du video pour alarme
 }
 
-condition = True
-
-while condition:
+while True:
+    # New instance for Chrome
     browser = webdriver.Chrome()
+    # Open the webpage
     browser.get('https://www.ppoletrangers.interieur.gouv.fr/?motif=rensej')
-
+    # Save the window opener (current window, do not mistaken with tab... not the same)
+    main_window = browser.current_window_handle
+    ################################################## Fill all the first form
     nom = browser.find_element_by_id("nom")
-    prenom = browser.find_element_by_id("prenom")
-    agdref = browser.find_element_by_id("agdref")
-    naissance = browser.find_element_by_id("naissance")
-    validite = browser.find_element_by_id("validite")
-
     nom.send_keys(data.get("nom"))
+    prenom = browser.find_element_by_id("prenom")
     prenom.send_keys(data.get("prenom"))
+    agdref = browser.find_element_by_id("agdref")
     agdref.send_keys(data.get("agdref"))
+    naissance = browser.find_element_by_id("naissance")
     naissance.send_keys(data.get("naissance"))
+    validite = browser.find_element_by_id("validite")
     validite.send_keys(data.get("validite"))
-
+    # Click in the submit button
     browser.find_element_by_xpath("//button[@type='submit']").click()
+    ##################################################
 
+    ################################################## Fill all the second form
     nationalite = Select(browser.find_element_by_name('nationalite'))
     nationalite.select_by_visible_text(data.get("nationalite"))
     arrdt = Select(browser.find_element_by_name('arrdt'))
     arrdt.select_by_visible_text(data.get("arrdt"))
-
+    # Click in the submit button
     browser.find_element_by_xpath("//input[@type='submit']").click()
+    ##################################################
 
+    # Text to find when there is an error
     text = "Nous ne sommes pas en mesure de traiter votre demande."
 
+    # If text error exist in webpage
     if (text in browser.page_source):
+        # Print in console that we didn't have the rd
+        print("Pas de chance pour trouver un rendez-vous ='(")
+        # Close the browser
         browser.quit()
+        # Retry in some seconds
+        time.sleep(data.get("time_in_secs"))
     else:
-        condition = False
-        #open tab
-        browser.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-        # Load a page 
-        browser.get('https://youtu.be/M3CSWLFL5u8')
+        # Open tab and load the video
+        browser.execute_script("window.open('"+ data.get("url_video") +"', '_blank')")
         break
-    time.sleep(data.get("time_in_secs"))
